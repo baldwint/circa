712dca409b889c7c9dd59c11f854ca464f1c6658
Thomas Baldwin <tbaldwin@uoregon.edu>
2015-02-03 12:32:10 -0800

    Simplify optimize program

--- circa/optimize.py
+++ circa/optimize.py
@@ -1,28 +1,19 @@
 import time
-#from wanglib.pylab_extensions.live_plot import plotgen
-#from wanglib.util import notraceback
 from .expt import do_count, configure_counter
 
-def gen_count_rate(pc, t=0.1):
+def gen_count_rate(t=0.1):
+    pc = configure_counter(duration=t)
     start = time.time()
     while True:
         y = do_count(*pc)/t
         yield time.time() - start, y
-
-def monitor_countrate(t=.1, ax=None, **kwargs):
-    with notraceback():
-        pc = configure_counter(duration=t)
-        with counting(*pc):
-            plotgen(gen_count_rate(pc, t), ax, **kwargs)
 
 if __name__ == "__main__":
     import wx
     from .monitor import MonitorWindow
 
     app = wx.App(False)
-    t = 0.1
-    pc = configure_counter(duration=t)
-    gen = gen_count_rate(pc, t)
+    gen = gen_count_rate(t=0.1)
     frame = MonitorWindow(None, 'Monitor', datagen=gen)
     frame.Show(True)
     app.MainLoop()
